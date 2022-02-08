457269747b846eff0aa68da2edd5346494921a21
hardikdheer <hardikdheer12@gmail.com>
2022-02-08 23:54:18 +0530

    Syntax error

--- index.js
+++ index.js
@@ -24,3 +24,34 @@ var a=10;
 
  var num=500
  console.log(`Half of ${num} is ${num/2}.`);
+
+ // let keyword is used
+let l = 100;
+console.log(l);
+
+// let l = "200"; //SyntaxError: Identifier 'l' has already been declared
+// console.log(l);
+
+//we can reinitialize a variable but we cannot redeclare it
+l = 300;
+console.log(l);
+
+//loops in JS
+var num = 10;
+
+// for (var i = 0; i < num; i++){
+//     if (i % 2 == 0) {
+//         console.log("num is even");
+//     }
+// }
+
+//const keyword
+const a = 2;
+console.log(a);
+// na hi redeclare na reinitialize
+// const a = 5;
+a = 4; //TypeError: Assignment to constant variable.
+console.log("hey");
+
+
+
